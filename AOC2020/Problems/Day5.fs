b8fa3b6ea470196f5efeb652047126a7ed21c011
Nathan <ntg9vz@virginia.edu>
2020-12-08 11:34:48 -0500

    fix day5 bug from refactor

--- AOC2020/Problems/Day5.fs
+++ AOC2020/Problems/Day5.fs
@@ -2,12 +2,9 @@ module AOC2020.Day5
 
 open System
 
-let parseBin (l: string) =
+let parseBin l =
   let toDigit = function | 'F' -> "0" | 'B' -> "1" | 'L' -> "0" | 'R' -> "1" | c -> failwithf "invalid char %c" c
-  let s =
-    l.ToCharArray()
-    |> Seq.map toDigit
-    |> String.concat ""
+  let s = Seq.map toDigit l |> String.concat ""
   Convert.ToInt32(s, 2)
 
 let problem1 (input: string array) =
@@ -18,7 +15,7 @@ let problem2 (input: string array) =
   let seats = input |> Seq.map parseBin |> Seq.sort
   let succ x = x + 1
   // compare seat + 1 with the next seat until we get a mismatch
-  let missing, _ = Seq.zip (Seq.map succ seats) (Seq.tail seats) |> Seq.find (fun (x, y) -> succ x <> y)
+  let missing, _ = Seq.zip (Seq.map succ seats) (Seq.tail seats) |> Seq.find (fun (x, y) -> x <> y)
 
   printfn "Missing seat ID: %i" missing
 
