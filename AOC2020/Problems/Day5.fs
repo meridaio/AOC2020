c9a7fed61f74873078b20b1a10a6c4f5643ef98b
Nathan <ntg9vz@virginia.edu>
2020-12-08 11:24:46 -0500

    big simplify day 5

--- AOC2020/Problems/Day5.fs
+++ AOC2020/Problems/Day5.fs
@@ -2,41 +2,24 @@ module AOC2020.Day5
 
 open System
 
-type Seat = {
-  Row: int
-  Column: int
-}
-
-let parseRowPartition = List.map (function | 'F' -> 0 | 'B' -> 1 | r -> failwithf "invalid row %c" r)
-let parseColPartition = List.map (function | 'L' -> 0 | 'R' -> 1 | c -> failwithf "invalid col %c" c)
-
-let intlToBinary (l: int list) =
-  let s = l |> Seq.map string |> String.concat ""
+let parseBin (l: string) =
+  let toDigit = function | 'F' -> "0" | 'B' -> "1" | 'L' -> "0" | 'R' -> "1" | c -> failwithf "invalid char %c" c
+  let s =
+    l.ToCharArray()
+    |> Seq.map toDigit
+    |> String.concat ""
   Convert.ToInt32(s, 2)
 
-let parseSeat (pass: string) =
-  let rs, cs = pass.ToCharArray() |> List.ofArray |> List.splitAt 7
-  {
-    Row = parseRowPartition rs |> intlToBinary
-    Column = parseColPartition cs |> intlToBinary
-  }
-
-let getSeatId (seat: Seat) =
-  seat.Row * 8 + seat.Column
-
 let problem1 (input: string array) =
-  let max = input |> Seq.ofArray |> Seq.map (parseSeat >> getSeatId) |> Seq.max
+  let max = input |> Seq.map parseBin |> Seq.max
   printfn "Max ID is %i" max
 
 let problem2 (input: string array) =
-  let seats =
-    input
-    |> Seq.ofArray
-    |> Seq.map (parseSeat >> getSeatId)
-    |> Seq.sort
-    |> List.ofSeq
+  let seats = input |> Seq.map parseBin |> Seq.sort
+  let succ x = x + 1
+  // compare seat + 1 with the next seat until we get a mismatch
+  let missing, _ = Seq.zip (Seq.map succ seats) (Seq.tail seats) |> Seq.find (fun (x, y) -> succ x <> y)
 
-  let missing, _ = Seq.zip [List.head seats..List.last seats] seats |> Seq.find (fun (x, y) -> x <> y)
   printfn "Missing seat ID: %i" missing
 
 type Solver () =
