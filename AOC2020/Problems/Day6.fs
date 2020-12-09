08b38ed590b19614df868cb1e6c90487f8ddb891
Nathan <ntg9vz@virginia.edu>
2020-12-08 19:56:27 -0500

    simplify some stuff day6

--- AOC2020/Problems/Day6.fs
+++ AOC2020/Problems/Day6.fs
@@ -6,27 +6,17 @@ open System
 let line = many1 (anyOf ['a'..'z']) .>> optional newline
 let lines = sepBy1 (many line) newline
 
-let getCount (input: string array) joiner =
+let getCount joiner (input: string array) =
   match run lines (String.Join("\n", input)) with
   | Success (result, _, _) -> result |> List.sumBy (List.map Set.ofList >> joiner >> Set.count)
-  | Failure (error, _, _) -> failwithf "%s" error
+  | Failure (error, _, _) -> failwith error
 
 let union = List.fold (Set.union) Set.empty
-
-let problem1 (input: string array) =
-  let totalCount = getCount input union
-  
-  printfn "Sum of counts: %i" totalCount
 
 let intersect (l: Set<char> list) =
   List.fold (Set.intersect) (List.head l) (List.tail l)
 
-let problem2 (input: string array) =
-  let totalCount = getCount input intersect
-
-  printfn "Sum of counts: %i" totalCount
-
 type Solver () =
-  static member Problem1 = problem1
-  static member Problem2 = problem2
+  static member Problem1 = getCount union >> printfn "Sum of counts: %i"
+  static member Problem2 = getCount intersect >> printfn "Sum of counts: %i"
   
